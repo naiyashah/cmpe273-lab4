f4138479834116d3bce50024f0f940a19bf4c3ac
naiyashah <shah.naiya8291@gmail.com>
2013-11-11 00:00:45 -0800

    app.js

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,31 +1,32 @@
-$(":button").click(function() {
-	var isbn = this.id;
-	var btnDisable = "#"+isbn;
-	
-	alert('About to report lost on ISBN ' + isbn);
-	$.ajax({
-		  url: 'http://localhost:8001/library/v1/books/'+isbn+'?status=lost',
-		  method: 'PUT',
-		  success: function() {
-			  $(btnDisable).attr("disabled","disabled");
-			  location.reload();
-		  },
-		  error: function() {
-		    alert("Can't Update")
-		  }
-		})
-	
+ $(document).ready(function(){
+    var elements = $('td[title="status"]');
+    for( i=0; i<elements.length; i++ ){
+            var newStatusIs = elements[i].innerHTML;
+            var numId = elements[i].id.slice("6");
+            var id = "#"+NumId;
+            if(newStatusIs == "lost")
+                    {
+                            $(id).attr("disabled","disabled");
+                    }
+            else{
+                    $(id).removeAttr("disabled");
+            }
+    }
 });
 
-
-$(document).ready(function(){
-	var row = document.getElementById("bookTable").rows.length;
-	for(i=0;i<row;i++)
-	{
-		var status=document.getElementById("bookTable").rows[i].cells[4].innerHTML;
-		if(status=="lost")
-		{
-			document.getElementById().disabled=true;
-		}
-	}
+$(":button").click(function() {
+        var isbn = this.id;
+        var uri = "/library/v1/books/"+isbn+"?status=lost";
+        var btnDisable = "#"+isbn;
+        var newStatus = "#status"+isbn;
+        alert('About to report lost on ISBN ' + isbn);
+        
+        $.ajax({
+                 url: uri,
+                 type: 'PUT',
+                 success: function(data) {
+                         $(btnDisable).attr("disabled","disabled");
+                         $(newStatus).text("lost");
+                 }
+                });
 });
